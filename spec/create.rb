bac007e9a164aa64ed162854b769d9827a2f41f1
Mark Plaksin <happy@mcplaksin.org>
2014-08-08 15:46:29 -0400

    Fix node create test.
    Still need to add correct output to test.  Maybe wait for JSON output.

--- spec/create.rb
+++ spec/create.rb
@@ -3,9 +3,8 @@ require_relative 'spec_helper'
 describe "Noodle" do
   it "should allow creating a node" do
     post '/nodes/jojo.example.com', params = '{"ilk":"host","status":"surplus","params":{"site":"moon"}}'
-    assert last_response.status == 201
-    # TODO: This doesn't do anything!
-    assert last_response.body = 'fixme'
+    assert_equal last_response.status, 201
+    assert_equal last_response.body, 'fixme'
   end
 end
 
