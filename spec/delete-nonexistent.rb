27215d691e2300e536dc0d11992b7f4802e81418
Mark Plaksin <happy@mcplaksin.org>
2014-08-08 23:00:00 -0400

    Fix tests.
    Oh!  All tests run in a single instance of the app.

--- spec/delete-nonexistent.rb
+++ spec/delete-nonexistent.rb
@@ -2,9 +2,9 @@ require_relative 'spec_helper'
 
 describe "Noodle" do
   it "should fail to delete a node that doesn't exist" do
-    delete '/nodes/jojo.example.com'
+    delete '/nodes/hoho.example.com'
     assert_equal last_response.status,422
-    assert last_response.body.must_include 'jojo.example.com does not exist'
+    assert last_response.body.must_include 'hoho.example.com does not exist'
   end
 end
 
