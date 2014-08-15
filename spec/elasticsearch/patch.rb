be4580417f2561d82b6b40fb6a0c670f04d025af
Mark Plaksin <happy@mcplaksin.org>
2014-08-14 20:58:26 -0400

    Fix patch test.
    Big lessons from this test exercise:
    
    - Need to refresh ES index to see new entries *immediately* after creating.
    
    - ES HTTP traffic logging (commented on spec_helper)

--- spec/elasticsearch/patch.rb
+++ spec/elasticsearch/patch.rb
@@ -3,16 +3,18 @@ require_relative 'spec_helper'
 describe "Noodle" do
   it "should patch a node" do
     put '/nodes/dodo.example.com', params = '{"ilk":"host","status":"surplus","params":{"site":"moon"}}'
+    assert_equal last_response.status,201
+    Node.gateway.refresh_index!
+
     patch '/nodes/dodo.example.com', params = '{"params":{"site":"mars"}}'
     assert_equal last_response.status,200
-    assert_equal last_response.body, <<EOF
-Name:   dodo.example.com
-Ilk:    host
-Status: surplus
-Params:
-  site = mars
-Facts:
-EOF
+    Node.gateway.refresh_index!
+
+    r = MultiJson.load last_response.body
+    assert_equal r['name'],           'dodo.example.com'
+    assert_equal r['ilk'],            'host'
+    assert_equal r['status'],         'surplus'
+    assert_equal r['params']['site'], 'mars'
   end
 end
 
