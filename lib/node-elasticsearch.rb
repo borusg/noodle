b67be18a8be5a2c5d488b1f7ab00a2dd46362e49
Mark Plaksin <happy@mcplaksin.org>
2014-08-18 21:02:29 -0400

    Node: Add fqdn and default to empty hashes.
    Plus comment/stub magic.

--- lib/node-elasticsearch.rb
+++ lib/node-elasticsearch.rb
@@ -5,10 +5,47 @@ class Node
     include Elasticsearch::Persistence::Model
 
     attribute :name,   String
+    attribute :fqdn,   String, default: :name
     # TODO: limit these two to a list of defaults
     attribute :ilk,    String
     attribute :status, String
-    attribute :facts,  Hashie::Mash, mapping: { type: 'object' }
-    attribute :params, Hashie::Mash, mapping: { type: 'object' }
+    attribute :facts,  Hashie::Mash, mapping: { type: 'object' }, default: {}
+    attribute :params, Hashie::Mash, mapping: { type: 'object' }, default: {}
+
+    # Magic:
+    #
+    # Make everything return either JSON or pretty text, defaul based on
+    # either user-agent or accepts or both or more :)
+    #
+    # Duplicate existing:
+    # hostname (partial or full)
+    # x=y
+    # x=~y
+    # @x=y
+    # x=
+    # x?
+    # x?=
+    # full
+    # json
+    # jmm :)  Maybe by some extensible plugin thing?
+    #
+    # New ideas:
+    # - Make precedence explicit:
+    #   - if both fact and param, param wins.
+    #   - And bare words are highest (or tunable?)
+    # - Make order explicit?  Needed?
+    # - ~x=y  That is, regexp on the fact/param name
+    # - barewords=paramname1[,paramname2,factname1,...] (needs better name)
+    #   Allow a list of fact/param names the values of which can be used 
+    #   as bare words in queries.
+    #
+    #   For example, if 'prodlevel' were in the list then 'prod'
+    #   could be used in a search to mean prodlevel=prod
+    def magic
+    end
+
+    def find_by_names(string)
+
+    end
 end
 
