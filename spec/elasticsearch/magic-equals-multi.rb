74c3121586a88d3665a172c77d23cb130131848f
Mark Plaksin <happy@mcplaksin.org>
2014-08-29 10:07:02 -0400

    Add 2nd magic-equals test.
    site=jupiter prodlevel=prod

---
+++ spec/elasticsearch/magic-equals-multi.rb
@@ -0,0 +1,17 @@
+require_relative 'spec_helper'
+
+describe "Noodle" do
+  it "should allow finding by 'TERM1=VALUE1 TERM2=VALUE2'" do
+    put '/nodes/koko.example.com', params = '{"ilk":"host","status":"enabled","params":{"site":"jupiter"}}'
+    assert_equal last_response.status, 201
+    put '/nodes/coco.example.com', params = '{"ilk":"host","status":"enabled","params":{"site":"jupiter","prodlevel":"prod"}}'
+    assert_equal last_response.status, 201
+
+    Node.gateway.refresh_index!
+
+    get '/nodes/_/site=jupiter%20prodlevel=prod'
+    assert_equal last_response.status, 200
+    assert_equal last_response.body, "coco.example.com\n"
+  end
+end
+
