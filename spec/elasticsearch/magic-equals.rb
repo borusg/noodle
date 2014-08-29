0d7c273e3faa5424bbf95a91e9f7b69ca4b8fc24
Mark Plaksin <happy@mcplaksin.org>
2014-08-28 22:03:42 -0400

    Add test for magic X=Y

---
+++ spec/elasticsearch/magic-equals.rb
@@ -0,0 +1,14 @@
+require_relative 'spec_helper'
+
+describe "Noodle" do
+  it "should allow finding by TERM=VALUE" do
+    put '/nodes/roro.example.com', params = '{"ilk":"host","status":"enabled","params":{"site":"jupiter"}}'
+    assert_equal last_response.status, 201
+    Node.gateway.refresh_index!
+
+    get '/nodes/_/site=jupiter'
+    assert_equal last_response.status, 200
+    assert last_response.body.must_include 'roro.example.com'
+  end
+end
+
