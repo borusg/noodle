58dfe5369377010441bdadf208e07eb713de69d2
Mark Plaksin <happy@mcplaksin.org>
2014-08-31 21:14:22 -0400

    Add Puppet ENC test; oops fix ?=

---
+++ spec/magic-puppet.rb
@@ -0,0 +1,21 @@
+require_relative 'spec_helper'
+require 'cgi'
+
+describe 'Noodle' do
+  it "should allow finding by hostname or FQDN and give Puppet ENC YAML output" do
+    put '/nodes/ioio.example.com', params = '{"ilk":"host","status":"enabled","params":{"site":"jupiter", "funky":"town"}}'
+    assert_equal last_response.status, 201
+    Node.gateway.refresh_index!
+
+    get "/nodes/_/ioio.example.com"
+    assert_equal last_response.status, 200
+    assert last_response.body.must_include '---
+classes:
+- baseclass
+parameters:
+  site: jupiter
+  funky: town
+'
+  end
+end
+
