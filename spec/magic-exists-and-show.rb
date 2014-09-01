cf0933ffbafbef3b13fcd5b9992c2a41a51482d4
Mark Plaksin <happy@mcplaksin.org>
2014-08-31 21:03:00 -0400

    Add test for ?=

---
+++ spec/magic-exists-and-show.rb
@@ -0,0 +1,15 @@
+require_relative 'spec_helper'
+require 'cgi'
+
+describe "Noodle" do
+  it "should allow finding and showing with TERM?=" do
+    put '/nodes/lolo.example.com', params = '{"ilk":"host","status":"enabled","params":{"site":"jupiter", "funky":"town"}}'
+    assert_equal last_response.status, 201
+    Node.gateway.refresh_index!
+
+    get "/nodes/_/funky#{CGI.escape('?=')}"
+    assert_equal last_response.status, 200
+    assert last_response.body.must_include 'yoyo.example.com funky=town'
+  end
+end
+
