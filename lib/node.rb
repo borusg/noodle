f75b2f760208762c4f1cc10e3936ff613a51e95b
Mark Plaksin <happy@mcplaksin.org>
2014-09-01 22:23:35 -0400

    Node::Search methods return self, shorten Node.all

--- lib/node.rb
+++ lib/node.rb
@@ -31,9 +31,7 @@ class Node
     end
 
     def self.all
-        search = Node::Search.new
-        search.all
-        body = search.go.results.collect{|hit| hit.name}.join("\n")
+        body = Node::Search.new.all.go.results.collect{|hit| hit.name}.join("\n")
         [body, 200]
     end
 
@@ -162,30 +160,37 @@ class Node::Search
 
     def initialize
         @query = []
+        self
     end
 
     def equals(term,value)
         @query << "(params.#{term}:#{value} OR facts.#{term}:#{value})"
+        self
     end
 
     def match(term,value)
         @query << "(params.#{term}:*#{value}* OR facts.#{term}:*#{value}*)"
+        self
     end
 
     def exists(term)
         @query << "(_exists_:params.#{term} OR _exists_:facts.#{term})"
+        self
     end
 
     def match_name(name)
         @query << "name:#{name}*"
+        self
     end
 
     def not_equal(term,value)
         @query << "-(params.#{term}:#{value} AND -facts.#{term}:#{value})"
+        self
     end
 
     def all
         @query << '*'
+        self
     end
 
     def go
