4d30c0db76c8de26d503f0e708d6740f81ca4480
Mark Plaksin <happy@mcplaksin.org>
2015-03-17 21:07:01 -0400

    Fix CLI spec and add two more

--- spec/cli-noodlin-param.rb
+++ spec/cli-noodlin-param.rb
@@ -1,9 +1,13 @@
 require_relative 'spec_helper'
 
 describe 'Noodle' do
-    it "should noodlin create" do
+    it "should noodlin param role=db" do
         hostname = HappyHelper::randomhostname
+
         noodlin = "create -s mars -i host -p hr -P prod #{hostname}"
+        assert_output(stdout="\n"){puts %x{bin/noodlin #{noodlin}}}
+
+        noodlin = "param role=db #{hostname}"
         assert_output(stdout="\n"){puts %x{bin/noodlin #{noodlin}}}
     end
 end
