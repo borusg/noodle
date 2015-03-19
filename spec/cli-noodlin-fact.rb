0efc4d16c544d3e4e164440aa24df3ba4b09b4d5
Mark Plaksin <happy@mcplaksin.org>
2015-03-18 21:53:13 -0400

    Add noodlin fact test, improve noodlin param test

--- spec/cli-noodlin-fact.rb
+++ spec/cli-noodlin-fact.rb
@@ -1,17 +1,18 @@
 require_relative 'spec_helper'
 
 describe 'Noodle' do
-    it "should noodlin param role=db" do
+    it "should noodlin fact role=db" do
         hostname = HappyHelper::randomhostname
 
         noodlin = "create -s mars -i host -p hr -P prod #{hostname}"
         assert_output(stdout="\n"){puts %x{bin/noodlin #{noodlin}}}
 
-        noodlin = "param role=db #{hostname}"
+        noodlin = "fact ram_gigs=2.0 #{hostname}"
         assert_output(stdout="\n"){puts %x{bin/noodlin #{noodlin}}}
 
-        # Make sure role param is present
-        noodle = "role= #{hostname}"
-        assert_output(stdout="#{hostname} role=[\"db\"]\n"){puts %x{bin/noodle #{noodle}}}
+        # Make sure ram_gigs fact is present
+        noodle = "ram_gigs= #{hostname}"
+        assert_output(stdout="#{hostname} ram_gigs=2.0\n"){puts %x{bin/noodle #{noodle}}}
+
     end
 end
