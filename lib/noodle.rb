976457bf6bd880ad79bbcd859460032fd77010d9
Mark Plaksin <happy@mcplaksin.org>
2015-03-30 10:48:52 -0400

    Change ES port if running on OpenShift

--- lib/noodle.rb
+++ lib/noodle.rb
@@ -167,12 +167,17 @@ require_relative 'noodle/node'
 require_relative 'noodle/search'
 require_relative 'noodle/option'
 
+# If OPENSHIFT_RUBY_IP is set, change ES port because can't use
+# default ES port at OpenShift
+#
 # TODO: This seems like a stupid spot for this stuff.
 # Plus should only do it if they don't exist.
+Noodle::Node.gateway.client = Elasticsearch::Client.new host: "#{ENV['OPENSHIFT_RUBY_IP']}:29200" if ENV['OPENSHIFT_RUBY_IP']
 Noodle::Node.gateway.create_index!
 Noodle::Node.gateway.refresh_index!
 #
 # Make sure at least some default options exist
+Noodle::Option.gateway.client = Elasticsearch::Client.new host: "#{ENV['OPENSHIFT_RUBY_IP']}:29200" if ENV['OPENSHIFT_RUBY_IP']
 Noodle::Option.gateway.create_index!
 Noodle::Option.new.save refresh: true # TODO Why save here but not above?
 Noodle::Option.gateway.refresh_index!
