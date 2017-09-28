aec7d33d4480e0caa25693377c29d774cf70a702
Mark Plaksin <happy@usg.edu>
2017-09-28 13:18:18 -0400

    oops, add new option.rb

---
+++ lib/noodle/option.rb
@@ -0,0 +1,45 @@
+require_relative 'client'
+
+# TODO: Is a separate class for this premature optimization?
+# TODO: Bubble up to target_ilk=default if not defined in the requested target_ilk?
+
+# ilk=option target_ilk=host uniqueness=fqdn
+# ilk=option target_ilk=sslcert uniqueness=fqdn
+# ilk=option target_ilk=samlcert uniqueness=fqdn,component_type (component_type would be shibboleth|asimba)
+class Noodle::Option
+  def self.get(ilk)
+    options = Noodle::Client.magic("ilk=option target_ilk#{ilk}")
+    unless options.empty?
+      return JSON.load()
+    else
+      # Hard-code some default options!
+      return {
+        'uniqueness'       => %w(ilk),
+        'allowed_statuses' => %w{enabled disabled future surplus},
+        'required_params'  => %w{ilk prodlevel project site status},
+        'default_ilk'      => 'host',
+        'default_status'   => 'enabled',
+        'bareword_terms'   => %w{prodlevel project site},
+        'limits'           => {
+          'project'   => %w{hr financials lms registration warehouse},
+          'prodlevel' => %w{dev preprod prod test},
+          # TODO: Shirley, these could be classes instead of strings.
+          'role'       => 'array',
+          'stack'      => 'array',
+          'gum'        => 'hash',
+          'site'       => %w{jupiter mars moon neptune pluto uranus},
+          'ilk'        => %w{host esx ucschassis ucsfi},
+          'status'     => %w{disabled enabled future surplus},
+        }
+      }
+    end
+  end
+
+  def self.option(ilk,option)
+    return self.get(ilk)[option]
+  end
+
+  def self.option_limit(ilk,thing)
+    return self.get(ilk)['limits'][thing]
+  end
+end
