0001f4361bca46a88539c38794131da69b273b65
Mark Plaksin <happy@usg.edu>
2017-10-03 14:49:25 -0400

    Merge options with default_options, JIC

--- lib/noodle/option.rb
+++ lib/noodle/option.rb
@@ -8,28 +8,29 @@ require_relative 'client'
 # ilk=option target_ilk=samlcert uniqueness=fqdn,component_type (component_type would be shibboleth|asimba)
 class Noodle::Option
   def self.get(target_ilk='default')
-    options = Noodle::Search.new(Noodle::Node).equals('ilk','option').equals('target_ilk',target_ilk).go
+    default_options = {
+      'uniqueness_params' => %w(ilk),
+      'required_params'   => %w{ilk prodlevel project site status},
+      'default_ilk'       => 'host',
+      'default_status'    => 'enabled',
+      'bareword_terms'    => %w{prodlevel project site},
+      'limits'            => {
+        'gum'       => 'hash',
+        'ilk'       => %w{host option esx ucschassis ucsfi},
+        'prodlevel' => %w{dev preprod prod test},
+        'project'   => %w{hr financials lms noodle registration warehouse},
+        'role'      => 'array',
+        'site'      => %w{jupiter mars moon neptune pluto uranus},
+        'stack'     => 'array',
+        'status'    => %w{disabled enabled future surplus},
+      }
+    }
+    options = Noodle::Search.new(Noodle::Node).equals('ilk','option').equals('target_ilk',target_ilk).go.results
     unless options.empty?
-      return JSON.load(options.results.to_json)
+      return JSON.load(default_options.merge(options.first).to_json)
     else
       # Hard-code some default options!
-      return {
-        'uniqueness_params' => %w(ilk),
-        'required_params'   => %w{ilk prodlevel project site status},
-        'default_ilk'       => 'host',
-        'default_status'    => 'enabled',
-        'bareword_terms'    => %w{prodlevel project site},
-        'limits'            => {
-          'gum'       => 'hash',
-          'ilk'       => %w{host option esx ucschassis ucsfi},
-          'prodlevel' => %w{dev preprod prod test},
-          'project'   => %w{hr financials lms noodle registration warehouse},
-          'role'      => 'array',
-          'site'      => %w{jupiter mars moon neptune pluto uranus},
-          'stack'     => 'array',
-          'status'    => %w{disabled enabled future surplus},
-        }
-      }
+      return default_options
     end
   end
 
