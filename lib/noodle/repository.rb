7d7ec4e71361dc7be696fee145fd9cbfff85ab19
Mark Plaksin <happy@mcplaksin.org>
2019-06-21 15:41:39 -0400

    Add Noodle::NodeRepository
    Repository pattern ahoy/step 1 of X

---
+++ lib/noodle/repository.rb
@@ -0,0 +1,36 @@
+class Noodle::NodeRepository
+  include Elasticsearch::Persistence::Repository
+  include Elasticsearch::Persistence::Repository::DSL
+
+  index_name 'noodle'
+  # TODO: ?
+  #document_type 'Noodle::Node'.to_sym
+  klass Noodle::Node
+
+  mapping do
+      indexes :name,   { index: 'not_analyzed' }
+      indexes :params, { type: 'object', dynamic: true, default: {} }
+      indexes :facts,  { type: 'object', dynamic: true, default: {} }
+  end
+
+  def serialize(node)
+      node.validate!
+  end
+
+  def deserialize(document)
+    node = super
+    node.id = document['_id']
+    node
+  end
+
+  def self.set_repository(repository)
+      @@repository = repository
+  end
+  def self.repository
+      @@repository
+  end
+
+  def all
+    search({ query: { match_all: { } } })
+  end
+end
