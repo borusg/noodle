0ead6a9c81b68d7bd77f5f68de5e668b0c61bc2d
Mark Plaksin <happy@usg.edu>
2020-05-22 14:08:34 -0400

    No really fix hashdig test.
    TIL "before do" !

--- spec/cli-hashdig.rb
+++ spec/cli-hashdig.rb
@@ -1,22 +1,25 @@
 require_relative 'spec_helper'
 
 describe 'Noodle' do
-  hostname_with_hash    = JSON.load(HappyHelper::node_hashdig)['facts']['fqdn']
-  hostname_without_hash = JSON.load(HappyHelper::node_hashdig_without_hash)['facts']['fqdn']
-  dotted_param = 'gum.address.zipcode'
-  param_value  = '90210'
-  dotted_fact  = 'chew.carrots.times'
-  fact_value   = '12'
 
-  node = HappyHelper::node_hashdig
-  fqdn = JSON.load(node)['facts']['fqdn']
-  put "/nodes/#{fqdn}?now", node
-  # assert_equal 201, last_response.status
+  before do
+    hostname_with_hash    = JSON.load(HappyHelper::node_hashdig)['facts']['fqdn']
+    hostname_without_hash = JSON.load(HappyHelper::node_hashdig_without_hash)['facts']['fqdn']
+    dotted_param = 'gum.address.zipcode'
+    param_value  = '90210'
+    dotted_fact  = 'chew.carrots.times'
+    fact_value   = '12'
 
-  node = HappyHelper::node_hashdig_without_hash
-  fqdn = JSON.load(node)['facts']['fqdn']
-  put "/nodes/#{fqdn}?now", node
-  # assert_equal 201, last_response.status
+    node = HappyHelper::node_hashdig
+    fqdn = JSON.load(node)['facts']['fqdn']
+    put "/nodes/#{fqdn}?now", node
+    assert_equal 201, last_response.status
+
+    node = HappyHelper::node_hashdig_without_hash
+    fqdn = JSON.load(node)['facts']['fqdn']
+    put "/nodes/#{fqdn}?now", node
+    assert_equal 201, last_response.status
+  end
 
   it "should hashdig param when BLAH=YABBA" do
     noodle = "hashdig=yep #{dotted_param}= #{hostname_with_hash}"
@@ -67,7 +70,6 @@ describe 'Noodle' do
     output = %x{bin/noodle #{noodle}}
     assert_output("#{hostname_without_hash}\n"){puts output}
   end
-  # this one fails:
   it "should hashdig fact when @BLAH=YABBA" do
     noodle = "hashdig=yep @#{dotted_fact}=#{fact_value}"
     output = %x{bin/noodle #{noodle}}
