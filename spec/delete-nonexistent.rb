664d11e4dbd1cd94d9a6eeee8478f5c82c1d5b99
Mark Plaksin <happy@usg.edu>
2020-06-19 14:57:04 -0400

    Update spec HTTP statuses based on recent changes

--- spec/delete-nonexistent.rb
+++ spec/delete-nonexistent.rb
@@ -3,7 +3,7 @@ require_relative 'spec_helper'
 describe 'Noodle' do
   it "should fail to delete a node that doesn't exist" do
     delete '/nodes/hoho.example.com'
-    assert_equal 422, last_response.status
+    assert_equal 424, last_response.status
     assert _(last_response.body).must_include 'hoho.example.com does not exist'
   end
 end
