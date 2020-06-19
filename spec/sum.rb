004c5d3e38ffc5250feb2b3f94e9ccb6636ef686
Mark Plaksin <happy@usg.edu>
2020-06-19 14:56:58 -0400

    sum test: Uniquify FQDN
    So this doesn't conflict with other tests.
    
    And yes, should fix all tests so they don't interact!

--- spec/sum.rb
+++ spec/sum.rb
@@ -3,7 +3,7 @@ require 'cgi'
 
 describe 'Noodle' do
   it "should allow sum to work" do
-    post '/nodes/zyyz.example.com?now', HappyHelper::node_jupiter(magnitude: 4)
+    post '/nodes/zqqz.example.com?now', HappyHelper::node_jupiter(magnitude: 4)
     assert_equal 201, last_response.status
     post '/nodes/zxxz.example.com?now', HappyHelper::node_jupiter(magnitude: 40)
     assert_equal 201, last_response.status
