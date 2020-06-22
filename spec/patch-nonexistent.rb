7d8e9697cf60435cb31411d7df4b345d2989b3c4
Mark Plaksin <happy@usg.edu>
2020-06-22 15:45:25 -0400

    Update patch-nonextistent test to include uniqueness params

--- spec/patch-nonexistent.rb
+++ spec/patch-nonexistent.rb
@@ -5,8 +5,9 @@ require_relative 'spec_helper'
 
 describe 'Noodle' do
   it 'fail to patch a node that doesn\'t exist' do
-    patch '/nodes/hoho.example.com', '{"site":"mars"}}'
-    assert_equal 423, last_response.status
-    assert _(last_response.body).must_include 'hoho.example.com does not exist'
+    params = { params: { ilk: 'host', site: 'mars' } }.to_json
+    patch '/nodes/hoho.example.com', params
+    assert_equal 400, last_response.status
+    assert _(last_response.body).must_include "Did not find exactly one match. Matches found: 0\n"
   end
 end
