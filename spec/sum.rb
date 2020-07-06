1142466bc023cbae90d459506be64fafb368c8f3
Mark Plaksin <happy@usg.edu>
2020-07-06 12:15:45 -0400

    Rubocop sum test

--- spec/sum.rb
+++ spec/sum.rb
@@ -3,12 +3,12 @@ require 'cgi'
 
 describe 'Noodle' do
   it 'allow sum to work' do
-    post '/nodes/zqqz.example.com?now', HappyHelper::node_jupiter(magnitude: 4)
+    post '/nodes/zqqz.example.com?now', HappyHelper.node_jupiter(magnitude: 4)
     assert_equal 201, last_response.status
-    post '/nodes/zxxz.example.com?now', HappyHelper::node_jupiter(magnitude: 40)
+    post '/nodes/zxxz.example.com?now', HappyHelper.node_jupiter(magnitude: 40)
     assert_equal 201, last_response.status
 
-    get '/nodes/_/ilk=host%20magnitude+'.gsub('+','%2B')
+    get '/nodes/_/ilk=host%20magnitude+'.gsub('+', '%2B')
     assert_equal 200, last_response.status
 
     assert _(last_response.body).must_include 'magnitude=44.0'
