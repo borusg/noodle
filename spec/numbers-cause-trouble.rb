c40b60973f32c2a3d05def7d5122b7c4cab14e68
Mark Plaksin <happy@usg.edu>
2020-07-06 12:15:17 -0400

    Numbers cause trouble test: Fix to use node without magnitude :)

--- spec/numbers-cause-trouble.rb
+++ spec/numbers-cause-trouble.rb
@@ -7,9 +7,9 @@ describe 'Noodle' do
   ten_dot_ten = '10.10.10.0'
   ten_dot_eleven = '10.11.0.0'
   it 'Make sure fqdn=10.10.10.0 does not also return 10.11.0.0' do
-    post "/nodes/#{ten_dot_ten}?now", HappyHelper.node_neptune
+    post "/nodes/#{ten_dot_ten}?now", HappyHelper.node_pluto
     assert_equal 201, last_response.status
-    post "/nodes/#{ten_dot_eleven}?now", HappyHelper.node_neptune
+    post "/nodes/#{ten_dot_eleven}?now", HappyHelper.node_pluto
     assert_equal 201, last_response.status
 
     get "/nodes/_/fqdn=#{ten_dot_ten}"
