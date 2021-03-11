681d04cc9f745eaf6b24df2699e0adaae6f691f4
Mark Plaksin <happy@usg.edu>
2021-03-11 07:55:42 -0500

    Set Puma workers to 0 when testing.
    To try to work around a GitLab CI error.

--- config/puma.rb
+++ config/puma.rb
@@ -1,3 +1,11 @@
+# Um, force workers to 0 for gitlab.com CI?
+#
+# GitLab CI gave this error but I can't tell what is trying to give the
+# -w argument to Puma:
+#
+# "Puma caught this error: option '-w' needs a parameter (Optimist::CommandlineError)"
+workers 0 if ENV['RACK_ENV'] == 'test'
+
 if ENV['RACK_ENV'] == 'production'
   stdout_redirect('/var/log/noodle/error.log', '/var/log/noodle/access.log', true)
   set_remote_address header: "X-Forwarded-For"
