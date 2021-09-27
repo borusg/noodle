842eff4ba92feb0dd5a7a89685007e2039b882a4
Mark Plaksin <happy@usg.edu>
2021-09-27 07:59:18 -0400

    Make patch test verify node was *created* correctly.

--- spec/patch.rb
+++ spec/patch.rb
@@ -5,14 +5,28 @@ require_relative 'spec_helper'
 
 describe 'Noodle' do
   it 'patch a node' do
+    # Create node
     post '/nodes/dodo.example.com?now', HappyHelper.node_moon
     assert_equal 201, last_response.status
 
+    # Verify the node was created correctly
+    params = { params: { ilk: 'host' } }.to_json
+    get '/nodes/dodo.example.com', params
+    assert_equal 200, last_response.status
+    r = MultiJson.load(last_response.body)
+    assert_equal 'dodo.example.com', r['name']
+    assert_equal 'host',             r['params']['ilk']
+    assert_equal 'surplus',          r['params']['status']
+    assert_equal 'moon',             r['params']['site']
+    assert_equal 'purple',           r['params']['color']
+    assert_equal 'jack',             r['facts']['thatsa']
+
+    # Patch, change site to mars
     params = { params: { ilk: 'host', site: 'mars' } }.to_json
     patch '/nodes/dodo.example.com?now', params
     assert_equal 200, last_response.status
 
-    # Verify PATCH returned the node we expected  
+    # Verify PATCH returned the node we expected
     r = MultiJson.load last_response.body
     assert_equal 'dodo.example.com', r['name']
     assert_equal 'host',             r['params']['ilk']
@@ -29,5 +43,7 @@ describe 'Noodle' do
     assert_equal 'host',             r['params']['ilk']
     assert_equal 'surplus',          r['params']['status']
     assert_equal 'mars',             r['params']['site']
+    assert_equal 'purple',           r['params']['color']
+    assert_equal 'jack',             r['facts']['thatsa']
   end
 end
