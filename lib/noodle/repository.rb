d50a156738980bed8d25a63ceedfb107282c7399
Mark Plaksin <happy@usg.edu>
2021-10-27 13:01:41 -0400

    Repository: Fix noodle_create_time

--- lib/noodle/repository.rb
+++ lib/noodle/repository.rb
@@ -103,7 +103,7 @@ class Noodle
     def serialize(document)
       node = super
       t = Time.now.utc.iso8601
-      node[:facts]['noodle_create_time'] = t if node[:facts]['create_time'].nil?
+      node[:facts]['noodle_create_time'] = t if node[:facts]['noodle_create_time'].nil?
       node[:facts]['noodle_update_time'] = t
       node
     end
