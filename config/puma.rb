26a6cd4b3b89f685ff7ef89b4ccb477b580b8ca5
Mark Plaksin <happy@usg.edu>
2021-11-18 13:43:00 -0500

    Lint puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -8,5 +8,5 @@ workers 0 if ENV['RACK_ENV'] == 'test'
 
 if ENV['RACK_ENV'] == 'production'
   stdout_redirect('/var/log/noodle/error.log', '/var/log/noodle/access.log', true)
-  set_remote_address header: "X-Forwarded-For"
+  set_remote_address header: 'X-Forwarded-For'
 end
