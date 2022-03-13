94370a7fd4decf1e80ec77eafb25500cafb51afb
Mark Plaksin <happy@usg.edu>
2022-03-13 12:04:40 -0400

    GitLab CI: Auth and HTTPS

--- bin/elasticsearch-create-noodle-user.sh
+++ bin/elasticsearch-create-noodle-user.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
-curl -s --cacert config/elastic-stack-cacert.pem -u "elastic:${ELASTICSEARCH_PASSWORD}" -X POST "https://localhost:9200/_security/user/noodle_user?pretty" -H 'Content-Type: application/json' -d'
+ca_file='config/elastic-stack-cacert.pem'
+if [ -f $ca_file ]
+then
+  tls_opt="--cacert ${ca_file}"
+else
+  tls_opt='-k'
+fi
+
+curl -s $tls_opt -u "elastic:${ELASTICSEARCH_PASSWORD}" -X POST "https://es01:9200/_security/user/noodle_user?pretty" -H 'Content-Type: application/json' -d'
 {
     "password" : "'${NOODLE_PASSWORD}'",
     "roles" : [ "noodle_role" ]
